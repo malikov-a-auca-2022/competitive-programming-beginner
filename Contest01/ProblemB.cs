1ca4619139cbd0bbf0cc5b1a1c99244391c0a2b7
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-10 16:21:55 +0600

    Update ProblemB.cs

--- Contest01/ProblemB.cs
+++ Contest01/ProblemB.cs
@@ -1,7 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class ProblemB {
-  static void Main() {
-  
-  }
+public class ProblemB
+{
+    static void solve()
+    {
+        Console.ReadLine();
+        string input = Console.ReadLine();
+        string[] inputtedNumbers = input.Split(' ');
+        int[] numbers = Array.ConvertAll(inputtedNumbers, s => int.Parse(s));
+        int res = numbers.Max() - numbers.Min();
+        Console.WriteLine(res);
+    }
+    static void Main ()
+    {
+        int numberOfTestCases = 1;
+        string input = Console.ReadLine();
+        int.TryParse(input, out numberOfTestCases);
+
+        while (numberOfTestCases > 0)
+        {
+            solve();
+            numberOfTestCases--;
+        }
+    }
 }
