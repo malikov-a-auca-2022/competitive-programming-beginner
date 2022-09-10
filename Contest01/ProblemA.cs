f4d9bdb10b4f925849ed91eb49da65176c8f4a9b
Adilet Malikov <malikov_a@auca.kg>
2022-09-10 14:17:32 +0600

    added solution of ProblemA

--- Contest01/ProblemA.cs
+++ Contest01/ProblemA.cs
@@ -1,8 +1,31 @@
 using System;
 
 public class ProblemA {
+  public static void solve() {
+
+    string input = Console.ReadLine();
+    if(input.Length % 2 != 0) {
+      Console.WriteLine("NO");
+      return;
+    }
+    for(int i = 0; i < input.Length / 2; i++ ) {
+      char start = input[i];
+      char middle = input[input.Length / 2 + i];
+      if(start != middle) {
+        Console.WriteLine("NO");
+        return;
+      }
+    }
+    Console.WriteLine("YES");
+  }
   static void Main() {
-    Console.WriteLine("Hello World!");
-    Console.ReadKey();
+    string input = Console.ReadLine();
+    int numberOfTestCases = 1;
+    int.TryParse(input, out numberOfTestCases);
+
+    while(numberOfTestCases > 0) {
+      solve();
+      numberOfTestCases--;
+    }
   }
 }
