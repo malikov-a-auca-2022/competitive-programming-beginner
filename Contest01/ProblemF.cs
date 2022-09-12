da13db26fa7f1466b80f4189f6b18561fa2a04e7
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-12 13:20:04 +0600

    Update ProblemF.cs

--- Contest01/ProblemF.cs
+++ Contest01/ProblemF.cs
@@ -2,6 +2,16 @@ using System;
 
 public class ProblemF {
   static void Main() {
-    
+    int result = 0;
+    string input = Console.ReadLine();
+    int numOfStatements = int.Parse(input);
+    for(int i = 1; i <= numOfStatements; i++) {
+      string statement = Console.ReadLine();
+      if(statement[1] == '+') 
+        result++;
+      else if(statement[1] == '-')
+        result--;
+    }
+    Console.WriteLine(result);
   }
 }
