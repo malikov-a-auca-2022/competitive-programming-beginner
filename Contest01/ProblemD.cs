fd1c20acfd7a1323a9110d8b59902220f7dc2ac6
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-12 13:01:36 +0600

    Update ProblemD.cs

--- Contest01/ProblemD.cs
+++ Contest01/ProblemD.cs
@@ -1,7 +1,33 @@
 using System;
 
 public class ProblemD {
+  public static int[] inputArray() {
+    string input = Console.ReadLine();
+    char[] inputtedNumbers = input.ToCharArray();
+    int[] numbers = Array.ConvertAll(inputtedNumbers, s => (int)char.GetNumericValue(s)); 
+    return numbers;
+  }
+  public static void solve() {
+    int lengthOfArray = int.Parse(Console.ReadLine());
+    int[] array1 = inputArray();
+    int[] array2 = inputArray();
+    for(int i = 0; i < lengthOfArray; i++)
+    {
+      if(array1[i] == 1 && array2[i] == 1) {
+        Console.WriteLine("NO");
+        return;
+      }
+    }
+    Console.WriteLine("YES");
+  }
   static void Main() {
-    
+    string input = Console.ReadLine();
+    int numberOfTestCases = 1;
+    int.TryParse(input, out numberOfTestCases);
+
+    while(numberOfTestCases > 0) {
+      solve();
+      numberOfTestCases--;
+    }
   }
 }
