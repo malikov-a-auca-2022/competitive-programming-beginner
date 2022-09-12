996a5c8cb7f2f8e313a808581ae11b1f34689bf0
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-12 13:08:55 +0600

    Update ProblemE.cs

--- Contest01/ProblemE.cs
+++ Contest01/ProblemE.cs
@@ -2,6 +2,9 @@ using System;
 
 public class ProblemE {
   static void Main() {
-    
+    string input = Console.ReadLine();
+    int weight = int.Parse(input);
+    if(weight % 2 == 0 && weight != 2) { Console.WriteLine("YES"); }
+    else {Console.WriteLine("NO");}
   }
 }
