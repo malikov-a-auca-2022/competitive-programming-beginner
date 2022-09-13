1b7ed18bec16be19a8da8523bd6305d0d2370b6d
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-13 15:38:51 +0600

    Update ProblemA.cs

--- Contest02/ProblemA.cs
+++ Contest02/ProblemA.cs
@@ -1,1 +1,34 @@
+using System;
 
+public class ProblemA {
+  
+  public static bool isPrime() {
+    int number = int.Parse(Console.ReadLine());
+    if (number == 1) { return false; }
+    if (number == 2) { return true; }
+    if (number % 2 == 0) { return false; }
+    
+    for(int i = 3; i < Math.Sqrt((double)number); i += 2) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  static void Main() {
+    string input = Console.ReadLine();
+    int numberOfTestCases = 1;
+    int.TryParse(input, out numberOfTestCases);
+
+    while(numberOfTestCases > 0) {
+      if(isPrime()) {
+        Console.WriteLine("yes");
+      } else {
+        Console.WriteLine("no");
+      }
+      numberOfTestCases--;
+    }
+  }
+  
+}
