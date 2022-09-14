6998ebaa078661cfa0b97ff1972673e6bda5ae14
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-14 16:04:22 +0600

    tried to fix problem
    input 16 - output 15

--- Contest02/ProblemB.cs
+++ Contest02/ProblemB.cs
@@ -4,14 +4,14 @@ class ProblemB {
 
   public static long sumOfDivisors(int number) {
     long result = 0;
-    for(int i = 1; i <= Math.Sqrt(number); i++) {
+    for(double i = 1.0; i <= Math.Sqrt(number); i++) {
       if(number % i == 0) {
         if(number / i == i || i == 1) {
-          result += i;
+          result += Convert.ToInt64(Math.Round(i));
           continue;
         }
-        result += i;
-        result += number / i;
+        result += Convert.ToInt64(Math.Round(i));
+        result += Convert.ToInt64(Math.Round(number / i));
       }
     }
     return result;
