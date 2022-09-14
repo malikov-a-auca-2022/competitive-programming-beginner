07bfc8fd58cbfb8f04f48ccc1a6609570b5efe97
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-14 15:42:35 +0600

    added solution of ProblemB Contest02

--- Contest02/ProblemB.cs
+++ Contest02/ProblemB.cs
@@ -1,1 +1,30 @@
+using System;
 
+class ProblemB {
+
+  public static long sumOfDivisors(int number) {
+    long result = 0;
+    for(int i = 1; i <= Math.Sqrt(number); i++) {
+      if(number % i == 0) {
+        if(number / i == i || i == 1) {
+          result += i;
+          continue;
+        }
+        result += i;
+        result += number / i;
+      }
+    }
+    return result;
+  }
+  
+  static void Main() {
+    int numberOfTestCases = 1;
+    numberOfTestCases = int.Parse(Console.ReadLine());
+
+    while(numberOfTestCases > 0) {
+      Console.WriteLine(sumOfDivisors(int.Parse(Console.ReadLine())));
+      numberOfTestCases--;
+    }
+  }
+  
+}
