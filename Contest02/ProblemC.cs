b91f6b9384b53ac67c00aca38eaff8683329dcc4
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-17 15:32:04 +0600

    fixed solution
    i finally found what was the reason of error

--- Contest02/ProblemC.cs
+++ Contest02/ProblemC.cs
@@ -1,48 +1,68 @@
 using System;
-public class ProblemC {
-  
-  public static bool[] isComposite = new bool[10^8 + 1];
-  public static int[] amountOfPrimeNumbers = new int[10^8 + 1];
-  
-  public static void EratosthenesSieve() {
-    isComposite[0] = true;
-    isComposite[1] = true;
-    for(int i = 2; i * i < isComposite.Length; i++) {
-      if(!isComposite[i]) {
-        for(int j = i * i; j < isComposite.Length; j += i) isComposite[j] = true;
-      }
+public class ProblemC
+{
+
+    public static bool[] isComposite = new bool[(int)Math.Pow(10, 8) + 1];
+    public static int[] amountOfPrimeNumbers = new int[(int)Math.Pow(10, 8) + 1];
+
+    public static void EratosthenesSieve()
+    {
+        isComposite[0] = true;
+        isComposite[1] = true;
+        for (int i = 2; i * i < isComposite.Length; i++)
+        {
+            if (!isComposite[i])
+            {
+                for (int j = i * i; j < isComposite.Length; j += i)
+                {
+                    isComposite[j] = true;
+                }
+            }
+        }
     }
-  }
-  
-  public static void CalculateAmountOfPrimeNumbers() {
-    for(int i = 2; i < isComposite.Length; i++) {
-      if(!isComposite[i]) {
-        amountOfPrimeNumbers[i] = amountOfPrimeNumbers[i-1] + 1;
-      }
+
+    public static void CalculateAmountOfPrimeNumbers()
+    {
+        for (int i = 2; i < isComposite.Length; i++)
+        {
+            if (!isComposite[i])
+            {
+                amountOfPrimeNumbers[i] = amountOfPrimeNumbers[i - 1] + 1;
+            } else
+            {
+                amountOfPrimeNumbers[i] = amountOfPrimeNumbers[i - 1];
+            }
+        }
     }
-  }
-  
-  public static double primeNumberTheorem(int number) {
-    return number / Math.Log(number);
-  }
-  
-  public static double errorPercent(int actualValue, double approximateValue) {
-    double result = Math.Abs((actualValue - approximateValue)) / actualValue;
-    return Math.Round(result * 100, 1);
-  }
-  
-  public static void Main() {
-    EratosthenesSieve();
-    CalculateAmountOfPrimeNumbers();
-    do {
-      int input = int.Parse(Console.ReadLine());
-      if(input == 0) {
-        break;
-      } else {
-        int actualValue = amountOfPrimeNumbers[input];
-        double approximateValue = primeNumberTheorem(input);
-        Console.WriteLine(errorPercent(actualValue, approximateValue));
-      }
-    } while(true);
-  }
+
+    public static double primeNumberTheorem(int number)
+    {
+        return number / Math.Log(number);
+    }
+
+    public static double errorPercent(int actualValue, double approximateValue)
+    {
+        double result = Math.Abs((actualValue - approximateValue)) / actualValue;
+        return Math.Round(result * 100, 1);
+    }
+
+    public static void Main()
+    {
+        EratosthenesSieve();
+        CalculateAmountOfPrimeNumbers();
+        do
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input == 0)
+            {
+                break;
+            }
+            else
+            {
+                int actualValue = amountOfPrimeNumbers[input];
+                double approximateValue = primeNumberTheorem(input);
+                Console.WriteLine(errorPercent(actualValue, approximateValue));
+            }
+        } while (true);
+    }
 }
