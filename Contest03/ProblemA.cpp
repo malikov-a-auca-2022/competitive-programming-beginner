cee67949073c88bcce1e9773dbfa990a2650d42e
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-21 17:04:21 +0600

    Update ProblemA.cpp

--- Contest03/ProblemA.cpp
+++ Contest03/ProblemA.cpp
@@ -1,1 +1,22 @@
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int numberOfRecipes; cin >> numberOfRecipes;
+    int min; cin >> min;
+    int questions; cin >> questions;
+    
+    int recipes [numberOfRecipes][2];
+    for(int i = 0; i < numberOfRecipes; i++) {
+        cin >> recipes[i][0];
+        cin >> recipes[i][1];
+    }
+
+    return 0;
+}
