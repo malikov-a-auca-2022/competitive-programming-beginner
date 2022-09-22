49514245465f1db5d20ea7bcf89886a6f31504e9
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-22 15:50:55 +0600

    First version with errors

--- Contest03/ProblemA.cpp
+++ Contest03/ProblemA.cpp
@@ -9,13 +9,41 @@ int main()
     cin.tie(0);
     
     int numberOfRecipes; cin >> numberOfRecipes;
-    int min; cin >> min;
-    int questions; cin >> questions;
+    int minToAllow; cin >> minToAllow;
+    int numberOfQuestions; cin >> numberOfQuestions;
+    int recipes [numberOfRecipes][2] = {{0}, {0}};
     
-    int recipes [numberOfRecipes][2];
     for(int i = 0; i < numberOfRecipes; i++) {
         cin >> recipes[i][0];
         cin >> recipes[i][1];
+    }
+    
+    // solve
+    int maxTemp = 1; int minTemp = 200000;
+    for(int i = 0; i < numberOfRecipes; i++) { //find min and max temperatures
+        for(int j = 0; j < 2; j++) {
+            int temp = recipes[i][j];
+            if(temp < minTemp) minTemp = temp;
+            if(temp > maxTemp) maxTemp = temp;
+        }
+    }
+    
+    int countAllowedTemperatures [maxTemp + 1] = {0}; //its length is max temperature + 1
+    
+    for(int i = 0; i < numberOfRecipes; i++) { //count allowed temperatures
+        for(int j = recipes[i][0]; j <= recipes[i][1]; j++) {
+            countAllowedTemperatures[j]++;
+        }
+    }
+    
+    for(int i = 0; i < numberOfQuestions; i++) { //answer to questions
+        int low; cin >> low;
+        int high; cin >> high;
+        int count = 0;
+        for(int j = low; j <= high; j++) {
+            if(countAllowedTemperatures[j] >= minToAllow) count++; 
+        }
+        cout << count << endl;
     }
 
     return 0;
