e091009ef6fb5e051a1c00000cf6040ce24dec1c
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-23 14:14:08 +0600

    sped it up little ibt

--- Contest03/ProblemB.cpp
+++ Contest03/ProblemB.cpp
@@ -17,19 +17,9 @@ int main()
         cin >> recipes[i][0];
         cin >> recipes[i][1];
     }
-    
-    // solve
-    int maxTemp = 1; int minTemp = 200000;
-    for(int i = 0; i < numberOfRecipes; i++) { //find min and max temperatures
-        for(int j = 0; j < 2; j++) {
-            int temp = recipes[i][j];
-            if(temp < minTemp) minTemp = temp;
-            if(temp > maxTemp) maxTemp = temp;
-        }
-    }
-    
-    int allowedTemperatures [maxTemp + 1] = {0}; //its length is max temperature + 1
-    bool isAllowed [maxTemp + 1] = {false};
+    int n = 2e5 + 1;
+    int allowedTemperatures [n] = {0};
+    bool isAllowed [n] = {false};
     
     for(int i = 0; i < numberOfRecipes; i++) { //count allowed temperatures
         for(int j = recipes[i][0]; j <= recipes[i][1]; j++) {
@@ -42,7 +32,7 @@ int main()
         int low; cin >> low;
         int high; cin >> high;
         int count = 0;
-        for(int j = low; j <= high && j <= maxTemp; j++) {
+        for(int j = low; j <= high; j++) { // j <= maxtemp
             if(isAllowed[j]) count++; 
         }
         cout << count << '\n';
