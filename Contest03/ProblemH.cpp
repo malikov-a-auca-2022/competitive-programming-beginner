4b4677c77ad6472773f112a51b53266f3ca16cad
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-09-27 17:04:38 +0600

    solved ProblemH. didn't pass test 2

---
+++ Contest03/ProblemH.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n; cin >> m;
+    unsigned long rooms[n]; 
+    rooms[n] = 0;
+    unsigned long pref[n];
+    int lo = 0, hi = n - 1;
+    int mid;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> rooms[i];
+        pref[i] = !i ? rooms[i] : pref[i - 1] + rooms[i];
+    }
+    
+    for(int i = 0; i < m; i++) {        //ask question and answer
+        lo = 0, hi = n - 1;
+        mid = (lo + hi) / 2;
+        unsigned long b; cin >> b;
+        while(true) {
+            mid = (lo + hi) / 2;
+            if(pref[mid] == b) {
+                cout << mid + 1 << " " << rooms[mid] << endl;
+                break; 
+            }
+            if(pref[mid + 1] > b && b > pref[mid]) {
+                cout << mid + 2 << " " << b - pref[mid] << endl;
+                break;
+            }
+            if(pref[mid] > b && b > pref[mid - 1]) {
+                cout << mid + 1 << " " << rooms[mid] - pref[mid] + b << endl;
+                break;
+            }
+            if(mid == lo) {
+                cout << mid + 1 << " " << b << endl;
+                break;
+            }
+            if(mid == hi) {
+                cout << mid + 1 << " " << rooms[mid] << endl;
+                break;
+            }
+            if(b > pref[mid]){
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        } 
+    }
+    
+    return 0;
+}
