ef361e3c236e6284ddba26e979c0291d9f8cc863
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-01 08:45:05 +0600

    solved ProblemH

--- Contest03/ProblemH.cpp
+++ Contest03/ProblemH.cpp
@@ -3,12 +3,12 @@
 
 using namespace std;
 void solve() {
-    unsigned long n, m;
+    long long n, m;
     cin >> n; cin >> m;
-    unsigned long rooms[n];
-    unsigned long pref[n];
-    long lo = 0, hi = n - 1;
-    long mid;
+    long long rooms[n];
+    long long pref[n];
+    long long lo = 0, hi = n - 1;
+    long long mid;
     
     for(int i = 0; i < n; i++) {
         cin >> rooms[i];
@@ -18,9 +18,13 @@ void solve() {
     for(int i = 0; i < m; i++) {        //ask question and answer
         hi = n - 1;
         mid = (lo + hi) / 2;
-        unsigned long b; cin >> b;
+        long long b; cin >> b;
         while(true) {
             mid = (lo + hi) / 2;
+            if(b == pref[n - 1]) {
+                cout << n << " " << rooms[n - 1] << '\n';
+                break;
+            }
             if(pref[mid] == b) {
                 cout << mid + 1 << " " << rooms[mid] << '\n';
                 break; 
