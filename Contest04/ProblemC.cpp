1937f13bc26a774faaf891a44dac178fd8136c81
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-06 14:52:45 +0600

    Create ProblemC.cpp

---
+++ Contest04/ProblemC.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    string s; cin >> s;
+    if(s.size() < 26) {
+        cout << "-1" << endl;
+        return;
+    }
+    for(int l = 0, r = )
+}
+
+int main()
+
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
