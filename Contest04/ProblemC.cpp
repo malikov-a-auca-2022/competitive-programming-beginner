548e856d7e8489b7cf7b081ff7201e86fc029256
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-06 16:18:00 +0600

    almost solved ProblemC

--- Contest04/ProblemC.cpp
+++ Contest04/ProblemC.cpp
@@ -2,18 +2,35 @@
 
 using namespace std;
 
+string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+string exam = "ABCABC??FGHIJK???OPQR?TUVWXY?";
+
 void solve() {
+    bool isIncluded[26] = {false};
     string s; cin >> s;
     if(s.size() < 26) {
         cout << "-1" << endl;
         return;
     }
-    for(int l = 0, r = )
+    int l = 0, r = 0;
+    for(r, l; r < s.size(); r++) {
+        for(int i = 0; i < 26; i++) {
+            if(s[r] == alph[i] && isIncluded[i]) { //this part is wrong
+                l = r;
+                break;
+            }
+            if(!isIncluded[i] && (s[r] == alph[i] || s[r] == '?')) {
+                s[r] = alph[i];
+                isIncluded[i] = true;
+                break;
+            }
+        }
+    }
+    if(r - l == 26) cout << s;
+    else cout << "-1";
 }
 
-int main()
-
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     solve();
