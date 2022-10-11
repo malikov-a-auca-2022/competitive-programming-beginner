1fea48ecd78ac82acf0b8ebc217524f8ff648988
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-11 15:10:26 +0600

    rewrote little bit ProblemC.cpp

--- Contest04/ProblemC.cpp
+++ Contest04/ProblemC.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 string alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 string alp2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZX";
-string ans2 = "ABABABBABCDEFGHIJKLMNABABABABAOPQRSTUVWXYABABABABAZ????????KLCSJB?????????Z";
 string exmp = "ABABABBAB????????????ABABABABA???????????ABABABABA?????????KLCSJB?????????Z";
 string cran = "ABABABBABAAAAAAAAAAAAABABABABAAAAAAAAAAAAABABABABADEFGHIMNOKLCSJBPQRTUVWXYZ";
 
 void solve2() {
     bool isIncluded[26] = {false};
-    bool isSolved = false, isSolvable = false;
+    bool isSolved = false;
     string s; cin >> s;
     if(s.size() < 26) {
         cout << "-1" << endl;
@@ -18,15 +17,14 @@ void solve2() {
     int l = 0, r = 0;
     string s2;
     int lCountInBool = 0, lCount = 0; 
-    for(l; l < s.size() - 25; l++) {
+    for(l; l < s.length() - 25; l++) {
         s2 = s.substr(l, 26);
         for(r = l; r < l + 25; r++) {
             for(int i = 0; i < 26; i++) { 
                 if(s[r] == alph[i]) {
                     if(isIncluded[i]) {
-                        for(int i = 0; i < 26; i++) isIncluded[i] = false;
+                        for(int i = 0; i < 26; i++) isIncluded[i] = false; //we need to move it
                         l = r;
-                        isSolvable = false;
                     } else {
                         lCount++;
                         isIncluded[i] = true;
@@ -41,56 +39,28 @@ void solve2() {
             }
         }
         if(lCountInBool == lCount) {
-            isSolvable = true;
-        }
-        if(!isSolvable) {
-            continue;
-        }
-        for(int i = 0; i < 26; i++) {
-            if(s2[i] == '?') {
-                for(int j = 0; j < 26; j++) {
-                    if(!isIncluded[j]) {
-                        s2[i] = alph[j];
-                        isIncluded[j] = true;
-                        break;
+            for(int i = 0; i < 26; i++) {
+                if(s2[i] == '?') {
+                    for(int j = 0; j < 26; j++) {
+                        if(!isIncluded[j]) {
+                            s2[i] = alph[j];
+                            isIncluded[j] = true;
+                            break;
+                        }
                     }
                 }
             }
-        }
-        s.replace(l, 26, s2);
-        for(int i = 0; i < s.size(); i++) {
-            if(s[i] == '?') {
-                s[i] == 'A';
+            s.replace(l, 26, s2);
+            for(int i = 0; i < s.size(); i++) {
+                if(s[i] == '?') {
+                    s[i] == 'A';
+                }
             }
+            isSolved = true;
+            break;
         }
-        isSolved = true;
-        break;
     }
     if(isSolved) cout << s;
-    else cout << "-1";
-}
-void solve() {
-    bool isIncluded[26] = {false};
-    string s; cin >> s;
-    if(s.size() < 26) {
-        cout << "-1" << endl;
-        return;
-    }
-    int l = 0, r = 0;
-    for(r, l; r < s.size(); r++) {
-        for(int i = 0; i < 26; i++) {
-            if(s[r] == alph[i] && isIncluded[i]) { //idk this method passes some testcases
-                l++;
-                break;
-            }
-            if(!isIncluded[i] && (s[r] == alph[i] || s[r] == '?')) {
-                s[r] = alph[i];
-                isIncluded[i] = true;
-                break;
-            }
-        }
-    }
-    if(r - l == 26) cout << s;
     else cout << "-1";
 }
 
