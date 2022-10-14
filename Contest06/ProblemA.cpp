47b98fb29c6bb6ab8a2d13c4c53307750ad57d06
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-14 16:35:12 +0600

    Create ProblemA.cpp

---
+++ Contest06/ProblemA.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    int a[n];
+    int v = n;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    sort(a, a + sizeof(a) / sizeof(a[0]));
+    
+    int i = 0;
+    while(a[i] * 2 <= a[n - 1]) { // && i < n / 2 mb
+        i++;
+    }
+    int j = 0;
+    while(a[j] <= a[i] && i < n) { //starting from biggest of less than half of actual biggest
+        v--;
+        j++;
+        i++;
+    }
+    int f = n;
+    for(int i = 0, j = n / 2; i < n / 2 && j < n; i++, j++) { //divide to two
+        if(a[i] * 2 <= a[j]) {
+            f--;
+        }
+    }
+    cout << v; // or f
+}
+
+int main()
+{   
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
