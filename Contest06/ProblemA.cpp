d8ce0786448ce047b55c01f96404d9465b62e6bd
malikov-a-auca-2022 <112888596+malikov-a-auca-2022@users.noreply.github.com>
2022-10-15 09:21:25 +0600

    Update ProblemA.cpp

--- Contest06/ProblemA.cpp
+++ Contest06/ProblemA.cpp
@@ -1,51 +1,30 @@
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-void solve() {
+ 
+int main() //passes til test 21 time limit
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     int n; cin >> n;
     int a[n];
-    int v = n;
+    bool b[n] = {false};
+    int r = n;
+    int l = n / 2; 
     for(int i = 0; i < n; i++) {
         cin >> a[i];
     }
     sort(a, a + sizeof(a) / sizeof(a[0]));
-    
-    int i = 0;
-    while(a[i] * 2 <= a[n - 1]) { // && i < n / 2 mb
-        i++;
-    }
-    int j = 0;
-    while(a[j] <= a[i] && i < n) { //starting from biggest of less than half of actual biggest
-        v--;
-        j++;
-        i++;
-    }
-    int f = n;
-    for(int i = 0, j = n / 2; i < n / 2 && j < n; i++, j++) { //divide to two
-        if(a[i] * 2 <= a[j]) {
-            f--;
+    for(int i = 0; i < n / 2; i++) {
+        for(int j = l; j < n; j++) {
+            if(a[i] * 2 <= a[j] && !b[j]) {
+                b[j] = true;
+                l = j + 1;
+                r--;
+                break;
+            }
         }
     }
-    cout << v; // or f
-    // third way is 
-//     int i = 0;
-//     while(a[i] * 2 <= a[n - 1]) {
-//         i++;
-//         v--;
-//     }
-   //fourth way
-    //idk search up in codeforces
-}
-
-int main()
-{   
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-        solve();
-    }
+    cout << r;
     return 0;
 }
